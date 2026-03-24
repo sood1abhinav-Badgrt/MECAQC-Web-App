93d94abe74c687eca7ae68a18cba74fc721bdc91
Abhinav sood <abhinavsood@Abhinavs-Laptop.local>
2026-03-24 12:53:24 -0500

    Started pydantic schema

---
+++ backend/schema.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import List
+
+class TechnologyInput(BaseModel):
+    name: str
+    scalePercent: float 
+
+class ScenarioInput(BaseModel):
+    state: str
+    sector: str
+    technologies: List[TechnologyInput]
+    timelineYears: int
+
+class ReductionOutput(BaseModel):
+    SO2TonsPerYear:float
+    NOxTonsPerYear:float
+    VOCTonsPerYear:float
+    CO2TonsPerYear:float
+
+class CostOutput(BaseModel):
+    SO2: float
+    NOx: float
+    VOC: float
+    CO2: float
+
+class ScenarioResult(BaseModel):
+    state: str
+    sector: str
+    timelineYears: int
+    reductions: ReductionOutput
+    costPerTon: CostOutput
+    summary: str
