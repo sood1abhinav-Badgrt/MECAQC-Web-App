f38bc20f969be45979f315c749cc7918274d1dcf
Abhinav sood <abhinavsood@Abhinavs-Laptop.local>
2026-03-27 16:11:29 -0500

    Current calculation logic: technologies chained together

--- backend/calculator.py
+++ backend/calculator.py
@@ -2,37 +2,37 @@ from mock_data import baselineEmissions, technologyEffectiveness, costPerTonDefa
 from schema import ReductionOutput, ScenarioResult, CostOutput
 
 def calculateScenario(inputScenario):
+    print("calculateScenario called")
     sector = inputScenario.sector
     baseline = baselineEmissions[sector]
-    totalReductions = {
-        "SO2": 0,
-        "NOx": 0,
-        "VOC": 0,
-        "CO2": 0
-    }   
-
     SO2Baseline = baseline["SO2"]
     NOxBaseline = baseline["NOx"]
     VOCBaseline = baseline["VOC"]
     CO2Baseline = baseline["CO2"]
-    timeFactor = inputScenario.timelineYears/ 10 #Choose 10 as a standard timeline base
+    totalReductions = {
+        "SO2": SO2Baseline,
+        "NOx": NOxBaseline,
+        "VOC": VOCBaseline,
+        "CO2": CO2Baseline
+    }   
+
+   
+    #timeFactor = inputScenario.timelineYears/ 10 #Choose 10 as a standard timeline base
 
     '''
-    Right now we assume that each technology is applied to the base
+    Right now we assume that each technology is stacked on each other
     Check to see what MECAQC actually uses
     '''
     for tech in inputScenario.technologies:
-        techSO2Reduction = SO2Baseline * technologyEffectiveness[tech.name]["SO2"] * (tech.scalePercent / 100) * timeFactor
-        totalReductions["SO2"] = totalReductions["SO2"] + techSO2Reduction
         
-        techNOxReduction = NOxBaseline * technologyEffectiveness[tech.name]["NOx"] * (tech.scalePercent / 100) * timeFactor
-        totalReductions["NOx"] = totalReductions["NOx"] + techNOxReduction
+        totalReductions["SO2"] = totalReductions["SO2"] * technologyEffectiveness[tech.name]["SO2"] * (tech.scalePercent / 100) 
         
-        techVOCReduction = VOCBaseline * technologyEffectiveness[tech.name]["VOC"] * (tech.scalePercent / 100) * timeFactor
-        totalReductions["VOC"] = totalReductions["VOC"] + techVOCReduction
+        totalReductions["NOx"] = totalReductions["NOx"] * technologyEffectiveness[tech.name]["NOx"] * (tech.scalePercent / 100) 
         
-        techCO2Reduction = CO2Baseline * technologyEffectiveness[tech.name]["CO2"] * (tech.scalePercent / 100) * timeFactor
-        totalReductions["CO2"] = totalReductions["CO2"] + techCO2Reduction
+
+        totalReductions["VOC"] = totalReductions["VOC"] * technologyEffectiveness[tech.name]["VOC"] * (tech.scalePercent / 100)
+        
+        totalReductions["CO2"] = totalReductions["CO2"] * technologyEffectiveness[tech.name]["CO2"] * (tech.scalePercent / 100) 
     
     emissionReductionOutput = ReductionOutput(
         SO2TonsPerYear = totalReductions["SO2"],
--- backend/routes/scenarios.py
+++ backend/routes/scenarios.py
@@ -1,34 +1,8 @@
 from schema import ScenarioInput, ScenarioResult, ReductionOutput, CostOutput
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+from calculator import calculateScenario
 
 router = APIRouter()
 @router.post("/scenario/run", response_model=ScenarioResult)
-def runScenario(inputData: ScenarioInput):
-    state = inputData.state
-    sector = inputData.sector
-    timelineYears = inputData.timelineYears
-
-    reductions = ReductionOutput(
-        SO2TonsPerYear = 4200,
-        NOxTonsPerYear = 2800,
-        VOCTonsPerYear = 640,
-        CO2TonsPerYear = 9100000
-    )
-
-    cost = CostOutput(
-        SO2 = 820,
-        NOx = 1100,
-        VOC = 950,
-        CO2 = 42
-    )
-
-    summary = f"This scenario reduces {state} emissions significantly."
-
-    return ScenarioResult(
-        state = state,
-        sector = sector,
-        timelineYears = timelineYears,
-        reductions = reductions,
-        costPerTon = cost,
-        summary = summary
-    )
+def runScenario(inputData: ScenarioInput = Body()):
+    return calculateScenario(inputData)
